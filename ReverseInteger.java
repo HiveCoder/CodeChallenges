1c503aed6c0281a73917df4048a55d4c36d4c60f
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-02-07 13:10:33 -0500

    Update ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,1 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10; // Get the last digit
+            x /= 10;             // Remove the last digit
+
+            // Check for overflow before multiplying by 10
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit; // Append the digit
+        }
+
+        return reversed;
+    }
+}
 
