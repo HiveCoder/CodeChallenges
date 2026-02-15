84fc460b2f926063ee4e0c8c98150791b30c35e6
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-02-15 18:30:22 -0500

    Update FindIndexFirst.java

--- FindIndexFirst.java
+++ FindIndexFirst.java
@@ -1,6 +1,25 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int n = haystack.length();
+        int m = needle.length();
+
+        // If needle is longer, impossible to match
+        if (m > n) return -1;
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+
+            // Compare characters one by one
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            // If we matched the whole needle
+            if (j == m) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
-
