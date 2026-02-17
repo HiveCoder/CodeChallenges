721e3bfdcad74aef9348d81905acc9d9293e5b39
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-02-16 23:15:09 -0500

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,19 +1,19 @@
-public class ClimbingStairs {
-
-    public static int climbStairs(int n) {
+class Solution {
+    public int climbStairs(int n) {
         // Base cases
-        if (n == 1) return 1;
-        if (n == 2) return 2;
+        if (n <= 2) {
+            return n;
+        }
 
         int first = 1;   // ways to reach step 1
         int second = 2;  // ways to reach step 2
-        int current = 0;
 
         for (int i = 3; i <= n; i++) {
-            current = first + second;
+            int current = first + second;
             first = second;
             second = current;
         }
 
         return second;
     }
+}
