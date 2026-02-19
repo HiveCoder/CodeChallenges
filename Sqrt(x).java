6de7544e08ae13b4e8ecb8720417f82f4aa27df6
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-02-18 20:19:42 -0500

    Update Sqrt(x).java

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -1,5 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        
+        if (x < 2) {
+            return x;  // sqrt(0)=0, sqrt(1)=1
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;  // prevent overflow
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                answer = mid;  // store potential answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
     }
 }
