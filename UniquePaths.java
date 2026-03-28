9a3f6ea3b521f0ea7d40c55349c755dc384b4514
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-03-28 14:24:21 -0400

    Update UniquePaths.java

--- UniquePaths.java
+++ UniquePaths.java
@@ -1,5 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        long res = 1;
+        int totalSteps = m + n - 2;
+        int k = Math.min(m - 1, n - 1); // choose smaller for efficiency
         
+        for (int i = 1; i <= k; i++) {
+            res = res * (totalSteps - k + i) / i;
+        }
+        
+        return (int) res;
     }
 }
