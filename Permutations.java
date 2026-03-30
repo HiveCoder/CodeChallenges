b7f91dabf227d0c2ea1cc4eb770994139a07c376
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-03-29 22:47:53 -0400

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, new ArrayList<>(), result);
+        return result;
+    }
+    
+    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        // Base case: permutation complete
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
         
+        for (int i = 0; i < nums.length; i++) {
+            // Skip if already used
+            if (used[i]) continue;
+            
+            // Choose
+            used[i] = true;
+            current.add(nums[i]);
+            
+            // Explore
+            backtrack(nums, used, current, result);
+            
+            // Un-choose (backtrack)
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
     }
 }
