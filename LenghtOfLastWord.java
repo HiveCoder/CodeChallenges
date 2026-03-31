d384014e6c8dba64bde6da70834b318a213dd3c6
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-03-31 04:34:59 -0400

    Update LenghtOfLastWord.java

--- LenghtOfLastWord.java
+++ LenghtOfLastWord.java
@@ -1,6 +1,19 @@
 class Solution {
-public:
-    int lengthOfLastWord(string s) {
-        
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces at the end of the string
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
     }
-};
+}
