826b2c9b72bd82a752494b98860dabcf83056889
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-07-08 15:53:20 -0400

    Implement maxProfit method for stock prices

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,5 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
+
+        int minimumPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int price : prices){
+
+            // Find cheapest day to buy
+            if(price < minimumPrice){
+                minimumPrice = price;
+            }
+
+            // Calculate profit if selling today
+            int profit = price - minimumPrice;
+
+            // Keep the best profit
+            if(profit > maxProfit){
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
     }
 }
