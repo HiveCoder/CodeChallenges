932ee52921fd681f4b266e6fcbf1069f277aa070
Hans Lloyd A. Reyes <130350653+HiveCoder@users.noreply.github.com>
2026-08-15 01:59:50 -0400

    Update CloneGraph.java

--- CloneGraph.java
+++ CloneGraph.java
@@ -1,27 +1,31 @@
-//Clone Graph Algorithm coded in Java
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
-}
-*/
-
 class Solution {
     public Node cloneGraph(Node node) {
-        
+        if (node == null) {
+            return null;
+        }
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        return dfs(node, map);
+    }
+
+    private Node dfs(Node node, HashMap<Node, Node> map) {
+        // If we've already cloned this node, return the clone
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // Create a clone of the current node
+        Node clone = new Node(node.val);
+
+        // Store it before visiting neighbors
+        map.put(node, clone);
+
+        // Clone all neighbors
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor, map));
+        }
+
+        return clone;
     }
 }
